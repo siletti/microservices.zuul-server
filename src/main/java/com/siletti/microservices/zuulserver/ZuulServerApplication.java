d7c0f25d9a6838728ea19d55fdf71f7cbb38776a
PcAlina <alberto.siletti@gmail.com>
2019-06-11 17:33:29 +0200

    add spring-cloud-starter-sleuth

--- src/main/java/com/siletti/microservices/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/siletti/microservices/zuulserver/ZuulServerApplication.java
@@ -1,9 +1,11 @@
 package com.siletti.microservices.zuulserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableZuulProxy
@@ -13,6 +15,12 @@ public class ZuulServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+    // x spring-cloud-starter-sleuth
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
